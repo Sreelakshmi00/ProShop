3659740639ff4830b021c94261279fc4109f05e6
Sreelakshmi00 <sreetp2000@gmail.com>
2021-04-13 16:38:44 +0530

    Basic Frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import HomeScreen from "./Screens/HomeScreen";
+import ProductScreen from "./Screens/ProductScreen";
+
+const App = () => {
+  return (
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Route path="/" component={HomeScreen} exact />
+          <Route path="/product/:id" component={ProductScreen} />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
+  );
+};
+
+export default App;
